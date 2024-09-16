036a6ee13702ead758c24c6764a51f152077f1fd
Tomislav Simovic <tomislav.simovic@yahoo.com>
2024-09-16 19:11:18 +0200

    fixing class Artikelbezeichnung

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 # Starting my Python Journey :D
 
-
 # trying to make a simple csv storage manager.
 
 
-import csv
+class Artikelbezeichnung:
+    def __init__(self, artikel=None):
+        self._artikel = artikel
 
-class StorageManager:
-    pass
+    def setze_artikel(self, neuer_artikel):
+        if neuer_artikel is None:
+            raise ValueError("Der Wert darf nicht None sein.")
+        if self.artikel is not None and neuer_artikel == self.artikel:
+            print(f"Der Artikel {neuer_artikel} ist bereits vorhanden.")
+        else:
+            self._artikel = neuer_artikel
+
+    @property
+    def artikel(self):
+        return self._artikel
+
+
+eingabe = Artikelbezeichnung('NDI')
+eingabe.setze_artikel("MTO")
+eingabe.setze_artikel("NDI")
+eingabe.setze_artikel("NDI")

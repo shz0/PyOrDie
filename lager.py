488ff0117b00c1fe2f9e30a58273d8b97e9370e3
Tomislav Simovic <tomislav.simovic@yahoo.com>
2024-09-16 19:20:18 +0200

    fixing class Artikelbezeichnung

---
+++ lager.py
@@ -0,0 +1,21 @@
+class Artikelbezeichnung:
+    def __init__(self, artikel=None):
+        self._artikel = artikel
+
+    def setze_artikel(self, neuer_artikel):
+        if neuer_artikel is None:
+            raise ValueError("Der Wert darf nicht None sein.")
+        if self.artikel is not None and neuer_artikel == self.artikel:
+            print(f"Der Artikel {neuer_artikel} ist bereits vorhanden.")
+        else:
+            self._artikel = neuer_artikel
+
+    @property
+    def artikel(self):
+        return self._artikel
+
+# testing
+eingabe = Artikelbezeichnung('NDI')
+eingabe.setze_artikel("MTO")
+eingabe.setze_artikel("NDI")
+eingabe.setze_artikel("NDI")
--- main.py
+++ main.py
@@ -3,24 +3,3 @@
 # trying to make a simple csv storage manager.
 
 
-class Artikelbezeichnung:
-    def __init__(self, artikel=None):
-        self._artikel = artikel
-
-    def setze_artikel(self, neuer_artikel):
-        if neuer_artikel is None:
-            raise ValueError("Der Wert darf nicht None sein.")
-        if self.artikel is not None and neuer_artikel == self.artikel:
-            print(f"Der Artikel {neuer_artikel} ist bereits vorhanden.")
-        else:
-            self._artikel = neuer_artikel
-
-    @property
-    def artikel(self):
-        return self._artikel
-
-# testing
-eingabe = Artikelbezeichnung('NDI')
-eingabe.setze_artikel("MTO")
-eingabe.setze_artikel("NDI")
-eingabe.setze_artikel("NDI")

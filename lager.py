a645d06b99caa2f33ed9c4ac8f399b87e9fea2fe
Tomislav Simovic <tomislav.simovic@yahoo.com>
2024-09-16 19:25:43 +0200

    fixing class Artikelbezeichnung

--- lager.py
+++ lager.py
@@ -3,7 +3,7 @@ class Artikelbezeichnung:
         self._artikel = artikel
         self._lagerort = lagerort
 
-    def setze_artikel(self, neuer_artikel):
+    def set_artikel(self, neuer_artikel):
         if neuer_artikel is None:
             raise ValueError("Der Wert darf nicht None sein.")
         if self.artikel is not None and neuer_artikel == self.artikel:
@@ -11,12 +11,21 @@ class Artikelbezeichnung:
         else:
             self._artikel = neuer_artikel
 
+    def set_lagerort(self, neuer_lagerort):
+        if neuer_lagerort is not None and not 1 <= neuer_lagerort <= 4:
+            raise ValueError('Lagerort 1 - 4 verfügbar')
+        self._lagerort = neuer_lagerort
+
     @property
     def artikel(self):
         return self._artikel
 
+    @property
+    def lagerort(self):
+        return self._lagerort
+
 # testing
 eingabe = Artikelbezeichnung('NDI')
-eingabe.setze_artikel("MTO")
-eingabe.setze_artikel("NDI")
-eingabe.setze_artikel("NDI")
+eingabe.set_artikel("MTO")
+eingabe.set_artikel("NDI")
+eingabe.set_artikel("NDI")

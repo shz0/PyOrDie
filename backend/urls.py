ae9b5706bc4ef39593c4fac7923977531e77f8bb
Tomislav Simovic <tomislav.simovic@yahoo.com>
2024-09-19 20:26:22 +0200

    working on django app

--- backend/urls.py
+++ backend/urls.py
@@ -16,7 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import JsonResponse
+
+# View
+def hallo(request):
+    return JsonResponse(
+        'Hallo Welt!', safe=False
+    )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', hallo),
 ]

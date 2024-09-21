299747e121feffef660badd3e224c3fea03dc967
Tomislav Simovic <tomislav.simovic@yahoo.com>
2024-09-21 13:23:06 +0200

    property solution for string to int func

--- helloDjango/models.py
+++ helloDjango/models.py
@@ -4,6 +4,7 @@ class Artikel(models.Model):
     titel = models.CharField(max_length=100)
     number = models.TextField(null=True, blank=True)
 
+    # Solution for my int/string problem!
     @property
     def number_as_int(self):
         if self.number:
@@ -18,3 +19,5 @@ class Artikel(models.Model):
         self.number = str(value)
         if value is not None else None:
             pass
+
+    # End of the Code for the Solution! (rewatch)

6ffa2653c525542840e56bc2473a0068bd4d4ac5
Tomislav Simovic <tomislav.simovic@yahoo.com>
2024-09-22 18:02:01 +0200

    working on backend

--- helloDjango/urls.py
+++ helloDjango/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.start, name='index'),
-    path('hallo/', views.index, name='hallo'),
-    path('navbar/', views.navbar, name='navbar')
+    path('',views.index, name='index'),
 ]
+
--- helloDjango/views.py
+++ helloDjango/views.py
@@ -1,13 +1,7 @@
 from django.shortcuts import render
 from . models import Artikel
 
+
 def index(request):
-    artikel = Artikel.objects.get(id=1)
-    return render(request, 'hallo.html', {'artikel': artikel})
-
-def start(request):
     artikels = Artikel.objects.all()
-    return render(request, 'start.html', {'artikels': artikels})
-
-def navbar(request):
-    return render(request, 'navbar.html')
+    return render(request, 'index.html', {'artikels': artikels})

93472ba2b829209aca0df3313599bcb4b9c3f0a2
Tomislav Simovic <tomislav.simovic@yahoo.com>
2024-09-22 17:58:51 +0200

    working on backend

--- helloDjango/urls.py
+++ helloDjango/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('hallo/', views.index, name='hallo'),
     path('',views.start, name='index'),
+    path('hallo/', views.index, name='hallo'),
     path('navbar/', views.navbar, name='navbar')
 ]
